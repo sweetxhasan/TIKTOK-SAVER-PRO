3e7d83abd2ee5d39fc43eff01e065249e650c553
sweetxhasan <hasanking25809560@gmail.com>
2025-11-24 12:28:27 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,14 @@ module.exports = {
       borderRadius: {
         'none': '0',
       },
+      borderWidth: {
+        '1': '1px',
+      },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in-out',
         'slide-up': 'slideUp 0.5s ease-out',
+        'toast-in': 'toastIn 0.3s ease-out',
+        'toast-out': 'toastOut 0.3s ease-in',
       }
     },
   },
