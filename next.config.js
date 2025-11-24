f401aa8d3dfc95add05e6755a108a2fc0e1b4292
sweetxhasan <hasanking25809560@gmail.com>
2025-11-24 11:42:23 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
   },
 }
 
