e188f41a7b876d699f6ee84487c11e46762b7bf1
sweetxhasan <hasanking25809560@gmail.com>
2025-11-24 12:35:01 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
 import '../styles/globals.css';
+import { useState } from 'react';
+import Toast from '../components/Toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [toast, setToast] = useState({
+    visible: false,
+    type: 'info',
+    title: '',
+    message: ''
+  });
+
+  const showToast = (toastData) => {
+    setToast({
+      ...toastData,
+      visible: true
+    });
+  };
+
+  const hideToast = () => {
+    setToast(prev => ({ ...prev, visible: false }));
+  };
+
+  return (
+    <>
+      <Component {...pageProps} onShowToast={showToast} />
+      <Toast toast={toast} onClose={hideToast} />
+    </>
+  );
 }
